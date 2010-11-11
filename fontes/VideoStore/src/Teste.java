4a0dd5b99a46e665955687e78d2c58e6f5c045f8
marcos.harbs <marcos.harbs@034fd7ed-ac8a-caa8-0fa0-561a791fd99f>
2010-11-11 01:26:47 +0000

    Projeto para teste da biblioteca.

--- fontes/VideoStore/src/Teste.java
+++
@@ -1,7 +0,0 @@
-import br.com.furb.engenhariasoftware.annotation.UseCase;
-
-
-@UseCase(id="UC001")
-public class Teste {
-
-}
---
+++ fontes/VideoStore/src/br/com/furb/engenhariasoftware/conceptproof/videostore/model/Esporte.java
@@ -0,0 +1,11 @@
+package br.com.furb.engenhariasoftware.conceptproof.videostore.model;
+
+public class Esporte extends Veiculo {
+
+	public Esporte(Integer numeroChassi, String numeroPlaca, String fabricante,
+			String modelo, Integer anoFabricacao, String cor) {
+		super(numeroChassi, numeroPlaca, fabricante, modelo, anoFabricacao, cor);
+		// TODO Auto-generated constructor stub
+	}
+
+}
