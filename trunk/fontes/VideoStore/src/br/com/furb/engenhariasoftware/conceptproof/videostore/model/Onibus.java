fa80b40f278885a3f0b0ca2e0e721bfd065da097
marcos.harbs <marcos.harbs@034fd7ed-ac8a-caa8-0fa0-561a791fd99f>
2010-11-11 01:26:47 +0000

    Projeto para teste da biblioteca.

---
+++ trunk/fontes/VideoStore/src/br/com/furb/engenhariasoftware/conceptproof/videostore/model/Onibus.java
@@ -0,0 +1,11 @@
+package br.com.furb.engenhariasoftware.conceptproof.videostore.model;
+
+public class Onibus extends Veiculo {
+
+	public Onibus(Integer numeroChassi, String numeroPlaca, String fabricante,
+			String modelo, Integer anoFabricacao, String cor) {
+		super(numeroChassi, numeroPlaca, fabricante, modelo, anoFabricacao, cor);
+		// TODO Auto-generated constructor stub
+	}
+
+}
